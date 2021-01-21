7cfcc5a8fe799b169287936a2b963a1a1a483faf
dongbohu <dongbo.hu@gmail.com>
2021-01-21 10:48:57 -0500

    Add mapping.py

---
+++ src/plugins/kegg/mapping.py
@@ -0,0 +1,60 @@
+def get_customized_mapping(cls):
+    mapping = {
+        "is_public": {
+            "type": "boolean"
+        },
+        "taxid": {
+            "type": "integer"
+        },
+        "genes": {
+            "properties": {
+                "mygene_id": {
+                    "normalizer": "keyword_lowercase_normalizer",
+                    "type": "keyword"
+                },
+                "ncbigene": {
+                    "normalizer": "keyword_lowercase_normalizer",
+                    "type": "keyword"
+                },
+                "ensemblgene": {
+                    "normalizer": "keyword_lowercase_normalizer",
+                    "type": "keyword"
+                },
+                "uniprot": {
+                    "normalizer": "keyword_lowercase_normalizer",
+                    "type": "keyword"
+                },
+                "symbol": {
+                    "type": "text"
+                }
+            }
+        },
+        "kegg": {
+            "properties": {
+                "id": {
+                    "normalizer": "keyword_lowercase_normalizer",
+                    "type": "keyword",
+                    "copy_to": [
+                        "all"
+                    ]
+                },
+                "database": {
+                    "normalizer": "keyword_lowercase_normalizer",
+                    "type": "keyword"
+                },
+                "entry": {
+                    "normalizer": "keyword_lowercase_normalizer",
+                    "type": "keyword"
+                },
+                "name": {
+                    "type": "text"
+                },
+                "organism_code": {
+                    "normalizer": "keyword_lowercase_normalizer",
+                    "type": "keyword"
+                }
+            }
+        }
+    }
+
+    return mapping
