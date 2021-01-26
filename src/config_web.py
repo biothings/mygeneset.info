c0e7f1f4f9d4ea13b735aa0b21d5a1b6b75cb3b8
Ricardo Avila <ravila@protonmail.com>
2021-01-26 15:51:23 -0800

    Fix species query handler.
    Create search function in pipeline.py and override build_string_query

--- src/config_web.py
+++ src/config_web.py
@@ -1,6 +1,6 @@
-
 import copy
 import re
+import logging
 from biothings.web.settings.default import APP_LIST, ANNOTATION_KWARGS, QUERY_KWARGS
 
 # *****************************************************************************
@@ -33,35 +33,29 @@ STATUS_CHECK = {
     'doc_type': 'geneset'
 }
 
+m
 # Customizations
 
-ANNOTATION_DEFAULT_SCOPES = [
-    '_id']
+ANNOTATION_DEFAULT_SCOPES = ['_id']
 
-TAX_REDIRECT = "http://t.biothings.io/v1/taxon/{0}?include_children=1"
+#TAX_REDIRECT = "http://t.biothings.io/v1/taxon/{0}?include_children=1"
 
-APP_LIST += [
-        (r"/{ver}/species/(\d+)/?", "tornado.web.RedirectHandler", {"url": TAX_REDIRECT}),
-        (r"/{ver}/taxon/(\d+)/?", "tornado.web.RedirectHandler", {"url": TAX_REDIRECT})
-        ]
+#APP_LIST += [
+#        (r"/{ver}/species/(\d+)/?", "tornado.web.RedirectHandler", {"url": TAX_REDIRECT}),
+#        (r"/{ver}/taxon/(\d+)/?", "tornado.web.RedirectHandler", {"url": TAX_REDIRECT})
+#        ]
 
 
 TAXONOMY = {
     "human": {"taxid": "9606"},
-    "homo sapiens": {"taxid": "9606"},
-
     "mouse": {"taxid": "10090"},
-    "mus musculus": {"taxid": "10090"},
-
     "fruitfly": {"taxid": "7227"},
-    "drosophila melanogaster": {"taxid": "7227"},
-
     "rat": {"taxid": "10116"},
     "nematode": {"taxid": "6239"},
     "zebrafish": {"taxid": "7955"},
     "thale-cress": {"taxid": "3702"},
     "dog": {"taxid": "9615"},
-    "horse": {"taxid": "9796"}
+    "horse": {"taxid": "9796"},
     "frog": {"taxid": "8364"},
     "pig": {"taxid": "9823"}
 }
@@ -90,15 +84,15 @@ SPECIES_TYPEDEF = {
 }
 
 
-DEFAULT_FIELDS = ['name', 'symbol', 'taxid']
-ANNOTATION_DEFAULT_SCOPES = ["_id"]
-
+DEFAULT_FIELDS = ['_id']
 
 ANNOTATION_KWARGS = copy.deepcopy(ANNOTATION_KWARGS)
 ANNOTATION_KWARGS['*'].update(SPECIES_TYPEDEF)
 
-QUERY_KWARGS['GET']['scopes']['default'] =  ["_id"]
-QUERY_KWARGS['POST']['scopes']['default'] =  ["_id"]
+QUERY_KWARGS = copy.deepcopy(QUERY_KWARGS)
+QUERY_KWARGS['*'].update(SPECIES_TYPEDEF)
+#QUERY_KWARGS['GET']['scopes']['default'] =  ["_id"]
+#QUERY_KWARGS['POST']['scopes']['default'] =  ["_id"]
 QUERY_KWARGS['*']['_source']['default'] = DEFAULT_FIELDS
 
 ES_QUERY_BUILDER = "web.pipeline.MyGenesetQueryBuilder"
