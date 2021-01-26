32c2085c8a0256ee1afdafd4639c868c0d740cb4
Ricardo Avila <ravila@protonmail.com>
2021-01-26 15:51:51 -0800

    Fix species query handler.
    Create search function in pipeline.py and override build_string_query

--- src/web/pipeline.py
+++ src/web/pipeline.py
@@ -1,6 +1,7 @@
 import re
 
 from biothings.utils.web.es_dsl import AsyncSearch
+from biothings.web.handlers.exceptions import BadRequest
 from biothings.web.pipeline import ESQueryBuilder
 
 
@@ -16,7 +17,23 @@ class MyGenesetQueryBuilder(ESQueryBuilder):
         search = self._extra_query_options(search, options)
         return search
 
+    def build_string_query(self, q, options):
+        search = super().build_string_query(q, options)
+        search = self._extra_query_options(search, options)
+        return search
+
+    def build_match_query(self, q, options):
+        search = super().build_match_query(q, options)
+        search = self._extra_query_options(search, options)
+        return search
+
     def _extra_query_options(self, search, options):
+        search = AsyncSearch().query( "function_score", query=search.query,
+                functions=[
+                    {"filter": {"term": {"taxid": 9606}}, "weight": "1.55"},  # human
+                    {"filter": {"term": {"taxid": 10090}}, "weight": "1.3"},  # mouse
+                    {"filter": {"term": {"taxid": 10116}}, "weight": "1.1"},  # rat
+                    ], score_mode="first")
         if options.species:
             search = search.filter('terms', taxid=options.species)
         return search
