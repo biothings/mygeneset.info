75d7874d2b7660a96cb294a2896f61876a486f37
Ricardo Avila <ravila@protonmail.com>
2022-01-21 12:07:23 -0800

    Refactor auth handlers

--- src/web/handlers/auth.py
+++ src/web/handlers/auth.py
@@ -1,86 +1,62 @@
 import json
 import logging
-from urllib.parse import urlencode
 
-import tornado.gen
+from biothings.web.auth.oauth_mixins import GithubOAuth2Mixin, OrcidOAuth2Mixin
 from biothings.web.handlers import BaseAPIHandler
-from tornado.auth import AuthError, OAuth2Mixin
-from tornado.httpclient import AsyncHTTPClient
 from tornado.httputil import url_concat
 
 
-def authenticated_user(func):
-    """
-    Decorator for UserGenesetHandler operations.
-    Ends request of user is not logged in.
-    """
-    def _(self, *args, **kwargs):
+class ORCIDLoginHandler(BaseAPIHandler, OrcidOAuth2Mixin):
+    SCOPES = ['/authenticate', 'openid']
+    CALLBACK_PATH = "/login/orcid"
 
-        if not self.current_user:
-            self.send_error(
-                message='You must log in first.',
-                status_code=401)
+    async def get(self):
+        CLIENT_ID = self.biothings.config.ORCID_CLIENT_ID
+        CLIENT_SECRET = self.biothings.config.ORCID_CLIENT_SECRET
+        redirect_uri = url_concat(self.request.protocol +
+                                    "://" + self.request.host +
+                                    self.CALLBACK_PATH,
+                                    {"next": self.get_argument('next', '/')})
+        code = self.get_argument('code', None)
+        if code is None:
+            logging.info('Redirecting to login...')
+            self.authorize_redirect(
+                redirect_uri=redirect_uri,
+                client_id=CLIENT_ID,
+                scope=self.SCOPES,
+            )
+        else:
+            logging.info("got code, try to get token")
+            token = await self.orcid_get_oauth2_token(client_id=CLIENT_ID,
+                                                      client_secret=CLIENT_SECRET,
+                                                      code=code)
+            # user = await self.orcid_get_authenticated_user_oidc(token)
+            user = await self.orcid_get_authenticated_user_record(token, token['orcid'])
+            user = self._format_user_record(user)
+            logging.info("Got user info: {}".format(user))
+            if user:
+                logging.info("Setting user cookie.")
+                self.set_secure_cookie("user", user)
+            else:
+                logging.info("Failed to get user info.")
+                self.clear_cookie("user")
+
+    def _format_user_record(self, user):
+        user_data = {}
+        user_data['login'] = user.get("orcid-identifier", {}).get("path")
+        if not user_data['login']:
             return
-        return func(self, *args, **kwargs)
-    return _
-
-class BaseAuthHandler(OAuth2Mixin, BaseAPIHandler):
-    """
-    Contains base Auth handler functionalities.
-    """
-    COOKIE_EXPIRATION = 15
-
-    async def get_authenticated_user(self, redirect_uri, client_id, client_secret,
-                               code, callback, login_method, extra_fields=None):
-        """Fetch authorization code if use has logged into the OAuth service."""
-
-        fields = set(["id", "login", "name", "email", "avatar_url"])
-        if extra_fields:
-            fields.update(extra_fields)
-
-        # Exchange code for access token
-        auth_body = {
-                "redirect_uri": redirect_uri,
-                "code": code,
-                "client_id": client_id,
-                "client_secret": client_secret,
-                "extra_params": extra_fields
-                }
-        if extra_fields:
-            auth_body.update(extra_fields)
-        http = AsyncHTTPClient()
-        response = await http.fetch(self._OAUTH_ACCESS_TOKEN_URL,
-                method="POST", body=urlencode(auth_body),
-                headers={'Content-Type': 'application/x-www-form-urlencoded',
-                         'Accept': 'application/json'})
-        if response.error:
-            raise AuthError("Auth error: {}".format(response.body))
-        access_token = json.loads(response.body)['access_token']
-
-        # Get user info from github
-        if login_method == 'github':
-            headers = {'Authorization': 'token {}'.format(access_token)}
-            response = await http.fetch("https://api.github.com/user",
-                                        method="GET", headers=headers)
-            user = json.loads(response.body)
-            user_fields  = ["login", "avatar_url", "name", "email", "company"]
-            user_data = {key: user.get(key) for key in user_fields}
-            return json.dumps(user_data)
-
-        # Get user info from orcid
-        elif login_method == 'orcid':
-            headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
-            orcid = json.loads(response.body)['orcid']
-            name = json.loads(response.body)['name']
-            # This request returns the user email (not strictly necessary for now)
-            response = await http.fetch("https://pub.orcid.org/v2.0/{}".format(orcid),
-                                        method="GET", headers=headers)
-            user = json.loads(response.body)
-            email = user.get("person").get("emails").get("email")
-            if len(email) >=1:
-                email = email[0]['email']
-            user_data = {"login": orcid, "name": name, "email": email}
-            return json.dumps(user_data)
+        # Only first name is required when registering for ORCID
+        first_name = user.get("person", {}).get("name", {}).get("given-names", {}).get("value")
+        last_name = user.get("person", {}).get("name", {}).get("family-name", {}).get("value")
+        user_data['name'] = first_name
+        if last_name:
+            user_data['name'] = first_name + " " + last_name
+        # email
+        email = user.get("person", {}).get("emails", {}).get("email")
+        if len(email) >=1:
+            user_data['email'] = email[0]['email']
+        return json.dumps(user_data)
 
     def get_current_user(self):
         user_json = self.get_secure_cookie("user")
@@ -89,118 +65,55 @@ class BaseAuthHandler(OAuth2Mixin, BaseAPIHandler):
         return json.loads(user_json.decode('utf-8'))
 
 
-class GitHubAuthHandler(BaseAuthHandler):
-    """
-    Handles the user authentication process using GitHub.
-    """
-    # Override settings from OAuth2Mixin
-    _OAUTH_AUTHORIZE_URL = "https://github.com/login/oauth/authorize"
-    _OAUTH_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token"
-    _USER_DATA_URL = "https://api.github.com/user"
+class GitHubLoginHandler(BaseAPIHandler, GithubOAuth2Mixin):
+    SCOPES = []
     CALLBACK_PATH = "/login/github"
-    SCOPE = "read:user"
 
-    @tornado.gen.coroutine
-    def get(self):
-        # we can append next to the redirect uri, so the user gets the
-        # correct URL on login
+    async def get(self):
+        CLIENT_ID = self.biothings.config.GITHUB_CLIENT_ID
+        CLIENT_SECRET = self.biothings.config.GITHUB_CLIENT_SECRET
+        code = self.get_argument('code', None)
         redirect_uri = url_concat(self.request.protocol +
-                                  "://" + self.request.host +
-                                  self.CALLBACK_PATH,
-                                  {"next": self.get_argument('next', '/')})
-
-        # if we have a code, we have been authorized so we can log in
-        if self.get_argument("code", False):
-            logging.info('Got authentication code.')
-            user = yield self.get_authenticated_user(
+                                   "://" + self.request.host +
+                                   self.CALLBACK_PATH,
+                                   {"next": self.get_argument('next', '/')})
+        if code is None:
+            logging.info('Redirecting to login...')
+            self.authorize_redirect(
                 redirect_uri=redirect_uri,
-                client_id=self.biothings.config.GITHUB_CLIENT_ID,
-                client_secret=self.biothings.config.GITHUB_CLIENT_SECRET,
-                code=self.get_argument("code"),
-                login_method="github",
-                callback=lambda: None
+                client_id=CLIENT_ID,
+                scope=self.SCOPES,
             )
+        else:
+            logging.info("got code, try to get token")
+            token = await self.github_get_oauth2_token(client_id=CLIENT_ID,
+                                                       client_secret=CLIENT_SECRET,
+                                                       code=code)
+            user = await self.github_get_authenticated_user(token['access_token'])
+            user = self._format_user_record(user)
+            logging.info("Got user info: {}".format(user))
             if user:
-                logging.info('logged in user from GitHub: %s', str(user))
                 logging.info("Setting user cookie.")
                 self.set_secure_cookie("user", user)
             else:
                 logging.info("Failed to get user info.")
                 self.clear_cookie("user")
-            self.redirect(self.get_argument("next", "/"))
+
+    def _format_user_record(self, user):
+        user_data = {}
+        user_data['login'] = user.get('login')
+        if not user_data['login']:
             return
+        if user.get('name'):
+            user_data['name'] = user['name']
+        if user.get('email'):
+            user_data['email'] = user['email']
+        if user.get('avatar_url'):
+            user_data['avatar_url'] = user['avatar_url']
+        return json.dumps(user_data)
 
-        # otherwise we need to request an authorization code
-        logging.info('Redirecting to login...')
-        yield self.authorize_redirect(
-            redirect_uri=redirect_uri,
-            client_id=self.biothings.config.GITHUB_CLIENT_ID,
-            extra_params={"scope": self.SCOPE, "foo": 1}
-        )
-
-
-class ORCIDAuthHandler(BaseAuthHandler):
-    """
-    Handles the user authentication process using ORCID.
-    """
-
-    # Override settings from OAuth2Mixin
-    _OAUTH_AUTHORIZE_URL = "https://orcid.org/oauth/authorize"
-    _OAUTH_ACCESS_TOKEN_URL = "https://orcid.org/oauth/token"
-    CALLBACK_PATH = "/login/orcid"
-    SCOPE = "/authenticate"
-
-    @tornado.gen.coroutine
-    def get(self):
-        # we can append next to the redirect uri, so the user gets the
-        # correct URL on login
-        redirect_uri = url_concat(self.request.protocol +
-                                  "://" + self.request.host +
-                                  self.CALLBACK_PATH,
-                                  {"next": self.get_argument('next', '/')})
-
-
-        # if we have a code, we have been authorized so we can log in
-        if self.get_argument("code", False):
-            user = yield self.get_authenticated_user(
-                redirect_uri=redirect_uri,
-                client_id=self.biothings.config.ORCID_CLIENT_ID,
-                client_secret=self.biothings.config.ORCID_CLIENT_SECRET,
-                code=self.get_argument("code"),
-                extra_fields={'grant_type': 'authorization_code'},
-                login_method="orcid",
-                callback=lambda: None
-            )
-            if user:
-                logging.info('logged in user from ORCID: %s', str(user))
-                logging.info("Setting user cookie.")
-                self.set_secure_cookie("user", user)
-            else:
-                self.clear_cookie("user")
-            self.redirect(self.get_argument("next", "/"))
-            return
-
-        # otherwise we need to request an authorization code
-        logging.info("Redirecting to login...")
-        yield self.authorize_redirect(
-            redirect_uri=redirect_uri,
-            client_id=self.biothings.config.ORCID_CLIENT_ID,
-            extra_params={"scope": self.SCOPE, "response_type": "code"}
-        )
-
-
-class UserInfoHandler(BaseAuthHandler):
-    """Handler for /user endpoint."""
-    def get(self):
-        current_user = self.get_current_user() or {}
-        for key in ['access_token', 'id']:
-            if key in current_user:
-                del current_user[key]
-        self.finish(current_user)
-
-
-class LogoutHandler(BaseAuthHandler):
-    """"Handler for /logout endpoint."""
-    def get(self):
-        self.clear_cookie("user")
-        self.redirect(self.get_argument("next", "/"))
+    def get_current_user(self):
+        user_json = self.get_secure_cookie("user")
+        if not user_json:
+            return None
+        return json.loads(user_json.decode('utf-8'))
