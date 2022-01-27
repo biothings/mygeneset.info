ce07f880847c013d2c67386a7d23d492defd8279
Ricardo Avila <ravila@protonmail.com>
2022-01-27 11:56:38 -0800

    Rename variable

--- src/web/authn/authn_provider.py
+++ src/web/authn/authn_provider.py
@@ -1,4 +1,5 @@
 import json
+
 from biothings.web.auth.authn import BioThingsAuthenticationProviderInterface
 
 
@@ -10,7 +11,7 @@ class UserCookieAuthProvider(BioThingsAuthenticationProviderInterface):
         self.cookie_name = cookie_name
 
     def get_current_user(self):
-        user_json = self.handler.get_secure_cookie(self.cookie_name)
-        if not user_json:
+        user = self.handler.get_secure_cookie(self.cookie_name)
+        if not user:
             return None
-        return json.loads(user_json.decode())
+        return json.loads(user.decode())
