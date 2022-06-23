fcd9a0b20fd327dc84e25b9e39122612621d6a1a
Ricardo Avila <ravila@protonmail.com>
2022-06-23 13:09:31 -0400

    Improve gene search by queryng all fields

--- src/plugins/msigdb/parser.py
+++ src/plugins/msigdb/parser.py
@@ -21,6 +21,7 @@ def parse_msigdb(data_file):
     TAXIDS = {
         "Homo sapiens": 9606,
         "Mus musculus": 10090,
+        "Rattus norvegicus": 10116
     }
     with open(data_file, 'r') as f:
         # File contains newline-delimited XML documents.
@@ -40,17 +41,16 @@ def parse_msigdb(data_file):
                 assert doc["taxid"] is not None, "Taxid not found: {}".format(data)
                 assert doc["taxid"] != "", "Taxid not found: {}".format(data)
                 # Look up gene IDs.DESCRIPTION_BRIEF Genes are stored in four different attributes in the data file:
-                # 1) MEMBERS contains a list of genes with their original identifier, which can be any of symbol, ensembl, entrez, or uniprot.
+                # 1) MEMBERS contains a list of genes with their original identifier, which can be any type of ID.
                 # 2) MEMBERS_SYMBOLIZED contains a list of genes converted to symbols.
-                # 3) MEMBERS_EZID contains a list of gene entrez IDs, but these have been converted from their original species to human.
+                # 3) MEMBERS_EZID contains a list of gene entrez IDs, but these have been converted from their corresponding human gene.
                 # 4) MEMBERS_MAPPING contains tuples of the three above IDs.
                 # We will use MEMBERS_MAPPNG to extract MEMBERS as the prefered id for lookup, followed by MEMBERS_SYMBOLIZED as backup.
                 members = [mapping.split(",")[0] for mapping in data["MEMBERS_MAPPING"].split("|")]
                 members_symbolized = [mapping.split(",")[1] for mapping in data["MEMBERS_MAPPING"].split("|")]
-                #members_symbolized = list(map(lambda x: x.replace(None, ""), members_symbolized))
                 gene_lookup = IDLookup(doc["taxid"])
-                gene_lookup.query_mygene(members, "entrezgene,ensembl.gene,uniprot,retired,accession")
-                gene_lookup.retry_failed_with_new_ids(members_symbolized, "symbol,alias")
+                gene_lookup.query_mygene(members, "all")
+                gene_lookup.retry_failed_with_new_ids(members_symbolized, "all")
                 genes = []
                 for i, j in zip(members, members_symbolized):
                     if gene_lookup.query_cache.get(i) is not None:
@@ -58,7 +58,7 @@ def parse_msigdb(data_file):
                     elif gene_lookup.query_cache.get(j) is not None:
                         genes.append(gene_lookup.query_cache[j])
                     else:
-                        logging.info("Could not find gene: {}".format(i))
+                        logging.info("Could not find gene {} with taxid {}".format(i, doc["taxid"]))
                 doc["genes"] = genes
                 # Additional msigdb data
                 msigdb = {}
@@ -91,4 +91,5 @@ if __name__ == "__main__":
     xmlfile = os.path.join(data_folder, "msigdb_v{}.xml".format(version))
     annotations = parse_msigdb(xmlfile)
     for a in annotations:
-        print(json.dumps(a, indent=2))
+        pass
+        #print(json.dumps(a, indent=2))
