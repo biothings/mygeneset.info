a5f9483f0842b94ad3f3a8a2ffc42c24fee24fba
Ricardo Avila <ravila@protonmail.com>
2022-06-29 22:11:41 -0400

    Bug fix: remove unnecessary iteration in gene lookup
    Gene lookup no longer uses members_symbolozed ids

--- src/plugins/msigdb/parser.py
+++ src/plugins/msigdb/parser.py
@@ -49,18 +49,15 @@ def parse_msigdb(data_file):
                 # 4) MEMBERS_MAPPING contains tuples of the three above IDs.
                 # We will use MEMBERS_MAPPNG to extract MEMBERS as the prefered id for lookup, followed by MEMBERS_SYMBOLIZED as backup.
                 members = [mapping.split(",")[0] for mapping in data["MEMBERS_MAPPING"].split("|")]
-                members_symbolized = [mapping.split(",")[1] for mapping in data["MEMBERS_MAPPING"].split("|")]
                 gene_lookup = IDLookup(doc["taxid"])
                 gene_lookup.query_mygene(members, "symbol,ensembl.gene,entrezgene,uniprot,reporter,refseq,alias")
                 gene_lookup.retry_failed_with_new_ids(members, "all")
                 genes = []
-                for i, j in zip(members, members_symbolized):
-                    if gene_lookup.query_cache.get(i) is not None:
-                        genes += gene_lookup.query_cache[i]
-                    elif gene_lookup.query_cache.get(j) is not None:
-                        genes += gene_lookup.query_cache[j]
+                for _id in members:
+                    if gene_lookup.query_cache.get(_id) is not None:
+                        genes += gene_lookup.query_cache[_id]
                     else:
-                        logging.info("Could not find gene {} with taxid {}".format(i, doc["taxid"]))
+                        logging.info("Could not find gene {} with taxid {}".format(_id, doc["taxid"]))
                 doc["genes"] = genes
                 # Additional msigdb data
                 msigdb = {}
