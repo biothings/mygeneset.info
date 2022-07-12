8732087337064c55357ee36d4bf25dcfe03e20e7
Ricardo Avila <ravila@protonmail.com>
2022-07-11 23:47:46 -0400

    Refactor gene query retry logic
    Condense query and retry steps into a single function. Use a tuple array as input instead of two separate lists.

--- src/utils/geneset_utils.py
+++ src/utils/geneset_utils.py
@@ -17,100 +17,128 @@ class IDLookup:
         Initialize with a list of ids and a species.
 
         >>> from biothings.utils.geneset_utils import IDLookup
-        >>> ids = ["ENSG0000012345", "ENSG0000012346"]
+        >>> ids = ["ENSG00000097007", "ENSG00000096968"]
         >>> taxid = 9606  # human
         >>> gene_lookup = IDLookup(taxid)
         >>> gene_lookup.query_mygene(ids, "ensembl.gene")
 
-        Retry failed queries with new ids.
+        Or, provide a list of tuples and list of id types for retry:
 
-        >>> new_ids = ["ABC123", "DEF456"]
-        >>> gene_lookup.retry_failed_with_new_ids(new_ids, "symbol")
+        >>> ids = [("ABL1", "ENSG00000097007"), ("JAK2", "ENSG00000096968")]
+        >>> taxid = 9606  # human
+        >>> gene_lookup = IDLookup(taxid)
+        >>> gene_lookup.query_mygene(ids, ["symbol", "ensembl.gene"])
 
-        Search query cache for hits.
+        Finally, search query cache for hits.
 
         >>> genes = []
         >>> for id in ids:
         >>>     if gene_lookup.query_cache.get(id):
-        >>>         # Hit can be a list or a single dict
         >>>         genes += gene_lookup.query_cache[id]
     """
+
     def __init__(self, species, cache_dict={}):
         self.species = species
         self.query_cache = cache_dict
 
-    def query_mygene(self, ids, id_type):
+    def query_mygene(self, ids, id_types):
         """Query information from mygene.info about each gene in 'ids'.
         Args:
-            ids (iterable): Array or set of gene ids to query.
-            id_type (str): query scope field for the ids.
+            ids: Array or set of gene ids to query. If genes are tuples,
+                the order is used to determine retry order.
+            id_types: query scope field for the ids.
                 Can be a comma-separated string for multiple scopes.
                 e.g. 'entrezgene,symbol'
+                Or a list of strings, to enable retries:
+                ['symbol', 'entrezgene']
         To search all fields, pass "all" as the id_type.
         """
-        self.ids = ids
-        mg = mygene.MyGeneInfo()
-        # Fields to query
-        fields = "entrezgene,ensembl.gene,uniprot.Swiss-Prot,symbol,name"
-        if id_type == "symbol":
-            scopes = "symbol,alias"
-        elif id_type == "entrezgene":
-            scopes = "entrezgene,retired"
+        # Some checks
+        if isinstance(id_types, list):
+            retry = True
+        elif isinstance(id_types, str):
+            retry = False
         else:
-            scopes = id_type
-        response = mg.querymany(ids,
-                                scopes=scopes,
-                                fields=fields,
-                                species=self.species,
-                                returnall=True)
-        # Save failed queries
-        self.missing = response['missing']
-        self.dups = response['dup']
-        # Format successful queries
-        for out in response['out']:
-            query = out['query']
-            if out.get('notfound'):
-                continue
-            gene = {'mygene_id': out['_id']}
-            if out.get('symbol') is not None:
-                gene['symbol'] = out['symbol']
-            if out.get('name') is not None:
-                gene['name'] = out['name']
-            if out.get('entrezgene') is not None:
-                gene['ncbigene'] = out['entrezgene']
-            if out.get('ensembl') is not None:
-                if len(out['ensembl']) > 1:
-                    for i in out['ensembl']:
-                        gene.setdefault('ensemblgene', []).append(i['gene'])
-                else:
-                    gene['ensemblgene'] = out['ensembl']['gene']
-            if out.get('uniprot') is not None:
-                gene['uniprot'] = out['uniprot']['Swiss-Prot']
-            gene = dict_sweep(gene)
-            gene = unlist(gene)
-            # Handle multiple hits by appending to list
-            if self.query_cache.get(query):
-                if isinstance(self.query_cache[query], list):
-                    self.query_cache[query].append(gene)
-                else:
-                    self.query_cache[query] = [self.query_cache[query], gene]
-            self.query_cache[query] = gene
+            raise TypeError("id_types must be a string or list")
 
-    def retry_failed_with_new_ids(self, new_ids, new_id_type):
-        """Retry failed queries with a new set of ids.
-        Only retries for ids that failed using 'self.query_mygene()'.
-        Args:
-            new_ids (iterable): is a list or set where the number of elements
-                matches the length and order of self.ids.
-            new_id_type (str): query scope for the new set of ids."""
-        assert len(new_ids) == len(self.ids), "New ids must have the same length as old ids."
-        retry_list = []
-        for e in self.missing:
-            indices = [i for i, x in enumerate(self.ids) if x == e]
-            retry_list = retry_list + [new_ids[i] for i in indices]
-        retry_list = list(set(retry_list))
-        if len(retry_list) == 0 or (len(retry_list) == 1 and retry_list[0] == ""):
-            logging.info("No ids to retry.")
+        if retry:
+            assert isinstance(ids[0], tuple), "To use retry, ids must be a list of tuples."
+            assert len(ids[0]) == len(id_types), "The size of each tuple must match the size of id_types"
+            retry_times = len(id_types) - 1
+            for n in ids:
+                assert isinstance(n, tuple), "To use retry, all ids must be tuples."
+                assert len(n) == len(id_types), "All tuples must be the same size."
         else:
-            logging.info("Retrying with {} ids: {}...".format(len(retry_list), retry_list[:10]))
-            self.query_mygene(retry_list, new_id_type)
+            retry_times = 0
+        failed_ids = []
+        current_try = 0
+        while current_try <= retry_times:
+            # Remove ids that already in the cache
+            if retry:
+                new_ids = [n for n in ids if n[current_try]
+                           not in self.query_cache.keys()]
+            else:
+                new_ids = [n for n in ids if n not in self.query_cache.keys()]
+            diff = len(ids) - len(new_ids)
+            assert diff >= 0, "This shouldn't have happened!"
+            if diff > 0:
+                logging.info(f"Found {diff} genes in query cache.")
+            ids = new_ids
+            # Generate params for query
+            if retry:
+                to_query = [n[current_try] for n in ids]
+                scopes = id_types[current_try]
+            else:
+                to_query = ids
+                scopes = id_types
+            # Query mygene.info
+            mg = mygene.MyGeneInfo()
+            fields_to_query = "entrezgene,ensembl.gene,uniprot.Swiss-Prot,symbol,name"
+            response = mg.querymany(to_query,
+                                    scopes=scopes,
+                                    fields=fields_to_query,
+                                    species=self.species,
+                                    returnall=True)
+            # Format successful queries
+            for out in response['out']:
+                query = out['query']
+                if out.get('notfound'):
+                    continue
+                gene = {'mygene_id': out['_id']}
+                if out.get('symbol') is not None:
+                    gene['symbol'] = out['symbol']
+                if out.get('name') is not None:
+                    gene['name'] = out['name']
+                if out.get('entrezgene') is not None:
+                    gene['ncbigene'] = out['entrezgene']
+                if out.get('ensembl') is not None:
+                    if len(out['ensembl']) > 1:
+                        for i in out['ensembl']:
+                            gene.setdefault('ensemblgene', []).append(i['gene'])
+                    else:
+                        gene['ensemblgene'] = out['ensembl']['gene']
+                if out.get('uniprot') is not None:
+                    gene['uniprot'] = out['uniprot']['Swiss-Prot']
+                gene = dict_sweep(gene)
+                gene = unlist(gene)
+                # Store results in cache
+                # Handle duplicate hits by appending to list
+                if self.query_cache.get(query):
+                    if isinstance(self.query_cache[query], list):
+                        self.query_cache[query].append(gene)
+                    else:
+                        self.query_cache[query] = [self.query_cache[query], gene]
+                else:
+                    self.query_cache[query] = gene
+            # Save failed queries
+            failed_ids = response['missing']
+            if len(failed_ids) == 0:
+                logging.info("No ids to retry.")
+                return
+            if retry and (current_try + 1 <= retry_times):
+                ids = [n for n in ids if n[current_try] in failed_ids]
+                assert len(ids) == len(failed_ids), "The length of these lists should match."
+                logging.info(f"Retrying {len(ids)} genes.")
+            else:
+                logging.info(f"Could not find {len(failed_ids)} genes.")
+            current_try += 1
