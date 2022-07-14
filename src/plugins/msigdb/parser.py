a61eba31b31c402f22b64b0c3f03aab875470daf
Ricardo Avila <ravila@protonmail.com>
2022-07-14 01:01:41 -0400

    Bug fix: edge cases where ids contain prefixes
    Some genesets contain IDs of different type from the other IDSs on the
    set, and are prefixed by a string with a pipe. e.g. 'ensg|' for ensembl.
    
    This is dealt with by removing prefixes from IDs that contain it, and
    making sure that all gene tuples are size 3.
    
    Also, if a geneset has mixed ID types, we fall back to searching all
    scopes.

--- src/plugins/msigdb/parser.py
+++ src/plugins/msigdb/parser.py
@@ -46,46 +46,66 @@ def parse_msigdb(data_file):
                 doc["is_public"] = True
                 assert doc["taxid"] is not None, "Taxid not found. ORGANISM missing in source data: {}".format(data)
                 assert doc["taxid"] != "", "Taxid not found. ORGANISM missing in source data: {}".format(data)
-                # MEMBERS contains a list of genes with their original identifier, which can be any type of ID.
-                # symbols contains a list of genes converted to symbols.
-                # MEMBERS_EZID contains a list of gene entrez IDs, but these have been converted from their corresponding human gene.
-                # MEMBERS_MAPPING contains tuples of the three above IDs.
-                members =  data["MEMBERS"].split(",")
-                symbols = [s.split(",")[1] for s in data["MEMBERS_MAPPING"].split("|")]
+                # Start a gene query
+                if doc["taxid"] != current_organism:
+                    current_organism = doc["taxid"]
+                    logging.info("Parsing msigdb data for organism {}".format(current_organism))
+                    gene_lookup = IDLookup(doc["taxid"])
+                # MEMBERS contains a "," delimited list of genes with their original identifier.
+                # MEMBERS_SYMBOLIZED contains a "," delimited list of genes converted to symbols (but sometimes containsother types of ids).
+                # MEMBERS_EZID contains a "," delimited list of entrez IDs, but these have been converted from their corresponding human gene.
+                # MEMBERS_MAPPING contains "|" delimited "," delimited tuples of the three above IDs.
+                # Figure out which scopes to use for the MEMBERS set
+                original_type = data.get("CHIP")
+                if original_type:
+                    if original_type.endswith("GENE_SYMBOL") or original_type == "HGNC_ID":
+                        members_scopes = 'symbol,alias'
+                    elif original_type.endswith("UniProt_ID"):
+                        members_scopes = 'uniprot'
+                    elif original_type.endswith("Ensembl_Gene_ID"):
+                        members_scopes = 'ensembl.gene'
+                    elif original_type.endswith("RefSeq"):
+                        members_scopes = 'refseq'
+                    elif original_type.endswith("NCBI_Gene_ID"):
+                        members_scopes = 'entrezgene,retired'
+                    elif original_type == "UniGene_ID":
+                        members_scopes = 'unigene'
+                    else:
+                        # default to all
+                        members_scopes = "symbol,ensembl.gene,entrezgene,uniprot,reporter,refseq,alias,unigene"
+                else:
+                    members_scopes = "symbol,ensembl.gene,entrezgene,uniprot,reporter,refseq,alias,unigene"
+                if "|" in data['MEMBERS']:
+                    # Sometimes ids contain a "|" with a prefix like 'ens', or 'linc'
+                    # to indicate the id type is different from the rest.
+                    # We can't use this to determine the scope, so we'll just use the default.
+                    members_scopes = "symbol,ensembl.gene,entrezgene,uniprot,reporter,refseq,alias,unigene"
+                # Prepare list of genes to query. We will query gene symbols and original ids.
+                members_mapping = [s.split(",") for s in data["MEMBERS_MAPPING"].split("|") if len(s.split(",")) == 3]
+                symbols = []
+                for id_tuple in members_mapping:
+                    if id_tuple[1] == "":
+                        symbols.append(id_tuple[0])  # If no symbol, use the original ID
+                    else:
+                        symbols.append(id_tuple[1])
+                members_raw =  data["MEMBERS"].split(",")
+                members = []
+                for m in members_raw:
+                    if "|" in m:
+                        # Remove prefixes like 'ens' or 'linc'
+                        members.append(m.split("|")[1])
+                    else:
+                        members.append(m)
                 if len(members) != len(symbols):
                     # This edge case shouldn't happen, but we can use another altnative
-                    members = [s.split(",")[0] for s in data["MEMBERS_MAPPING"].split("|")]
+                    members = [s.split(",")[0] for s in data["MEMBERS_MAPPING"].split("|") if len(s.split(",")) == 3]
                 assert len(members) == len(symbols), "ID lists are not the same length: {} {}".format(
                     len(members), len(symbols))
                 # Using symbols as the preferred id, because it consistently gives the most hits across datasets
                 id_list = list(zip(symbols, members))
-                if doc["taxid"] != current_organism:
-                    # Start a new query cache
-                    current_organism = doc["taxid"]
-                    logging.info("Parsing msigdb data for organism {}".format(current_organism))
-                    gene_lookup = IDLookup(doc["taxid"])
-                # Figure out which scopes to use for the retry query
-                original_type = data.get("CHIP")
-                if original_type:
-                    if original_type.endswith("GENE_SYMBOL") or original_type == "HGNC_ID":
-                        scopes = 'symbol,alias'
-                    elif original_type.endswith("UniProt_ID"):
-                        scopes = 'uniprot'
-                    elif original_type.endswith("Ensembl_Gene_ID"):
-                        scopes = 'ensembl.gene'
-                    elif original_type.endswith("RefSeq"):
-                        scopes = 'refseq'
-                    elif original_type.endswith("NCBI_Gene_ID"):
-                        scopes = 'entrezgene,retired'
-                    elif original_type == "UniGene_ID":
-                        scopes = 'unigene'
-                    else:
-                        scopes = "symbol,ensembl.gene,entrezgene,uniprot,reporter,refseq,alias,unigene"
-                else:
-                    scopes = "symbol,ensembl.gene,entrezgene,uniprot,reporter,refseq,alias,unigene"
                 # Run query
                 logging.info("Queryng genes for geneset #{}: {}".format(geneset_index, doc["_id"]))
-                gene_lookup.query_mygene(id_list, ['symbol,alias', scopes])
+                gene_lookup.query_mygene(id_list, ['symbol,alias', members_scopes])
                 query_results = gene_lookup.get_results(id_list)
                 doc.update(query_results)  # Merge doc with query results
                 # Additional msigdb data
