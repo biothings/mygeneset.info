1f932d03a5f6073f9d4d2a0a422d2f6e67e263e6
Ricardo Avila <ravila@protonmail.com>
2022-07-20 18:42:00 -0400

    Update mapping

--- src/plugins/reactome/mapping.py
+++ src/plugins/reactome/mapping.py
@@ -12,9 +12,16 @@ def get_customized_mapping(cls):
         "taxid": {
             "type": "integer"
         },
+        "count": {
+            "type": "integer"
+        },
         "genes": {
             "properties": {
                 "mygene_id": {
+                    "normalizer": "keyword_lowercase_normalizer",
+                    "type": "keyword"
+                },
+                "source_id": {
                     "normalizer": "keyword_lowercase_normalizer",
                     "type": "keyword"
                 },
@@ -36,6 +43,35 @@ def get_customized_mapping(cls):
                 },
                 "name": {
                     "type": "text"
+                }
+            }
+        },
+        "duplicates": {
+            "properties": {
+                "ids": {
+                    "properties": {
+                        "id": {
+                            "normalizer": "keyword_lowercase_normalizer",
+                            "type": "keyword"
+                        },
+                        "count": {
+                            "type": "integer"
+                        }
+                    }
+                },
+                "count": {
+                    "type": "integer"
+                }
+            }
+        },
+        "not_found": {
+            "properties": {
+                "ids": {
+                    "normalizer": "keyword_lowercase_normalizer",
+                    "type": "keyword"
+                },
+                "count": {
+                    "type": "integer"
                 }
             }
         },
