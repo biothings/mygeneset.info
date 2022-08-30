ef9a549bebfd0be280c19c6ba364c1dbcd2d7309
Ricardo Avila <ravila@protonmail.com>
2022-08-30 14:15:07 -0400

    Enable headless mode

--- src/tests/bot.py
+++ src/tests/bot.py
@@ -7,7 +7,7 @@ class Bot:
     def start(self):
         """Start a selenium web driver"""
         chrome_options = webdriver.ChromeOptions()
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--lang=en")
