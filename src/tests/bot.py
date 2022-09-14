e588f234a7d098486f572b1fcaff78ca0385321e
Ricardo Avila <ravila@protonmail.com>
2022-09-14 12:15:30 -0400

    Switch to Firefox

--- src/tests/bot.py
+++ src/tests/bot.py
@@ -1,18 +1,15 @@
-from seleniumwire import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from webdriver_manager.chrome import ChromeDriverManager, ChromeType
+from seleniumwire import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 class Bot:
     def start(self):
         """Start a selenium web driver"""
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--lang=en")
-        driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        self.driver = webdriver.Chrome(driver_path, options=chrome_options)
+        firefox_options = webdriver.FirefoxOptions()
+        firefox_options.headless = True
+        self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(),
+                                        options=firefox_options)
         self.driver.implicitly_wait(20)
 
     def stop(self):
