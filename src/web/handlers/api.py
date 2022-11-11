3d008c8bf33ca8e241e03cd4321cb4e88d55bab8
Ricardo Avila <ravila@protonmail.com>
2022-11-11 17:15:37 -0500

    Add custom short _ids for user genesets.

--- src/web/handlers/api.py
+++ src/web/handlers/api.py
@@ -3,6 +3,7 @@ API handler for MyGeneset submit/ endpoint
 """
 
 import json
+import random
 from datetime import datetime, timezone
 
 import elasticsearch
@@ -11,6 +12,7 @@ from biothings.web.auth.authn import BioThingsAuthnMixin
 from biothings.web.handlers import BaseAPIHandler
 from biothings.web.handlers.query import BiothingHandler, QueryHandler
 from tornado.web import HTTPError
+
 from utils.mygene_lookup import MyGeneLookup
 
 
@@ -41,6 +43,7 @@ class UserGenesetHandler(BioThingsAuthnMixin, BaseAPIHandler):
     def prepare(self):
         super().prepare()
         # Enable XSRF protection for POST requests when user is not authenticated
+        # I disabled this since there are issues implementing this with the frontend.
         # if self.request.method == "POST" and not self.current_user:
         #    self.check_xsrf_cookie()
 
@@ -54,6 +57,16 @@ class UserGenesetHandler(BioThingsAuthnMixin, BaseAPIHandler):
                 return
             return func(self, *args, **kwargs)
         return _
+
+    def _generate_geneset_id(self):
+        """Generate short random geneset ids."""
+        # Generate six random integers between 0 and 61
+        random_ints = [random.randint(0, 61) for _ in range(6)]
+        # Convert to BASE62
+        # I have replaced the ambiguous characters "0Ol1oI" from the alpabet with "@&=$-*""
+        alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        base62str = "".join([alphabet[i] for i in random_ints])
+        return f"mygst:{base62str}"
 
     async def _get_geneset(self, _id):
         """Fetch a geneset document from Elasticsearch"""
@@ -131,7 +144,7 @@ class UserGenesetHandler(BioThingsAuthnMixin, BaseAPIHandler):
                 raise HTTPError(400, reason="Body element 'genes' must be a list.")
         elif request_type == 'PUT':
             if payload.get("genes") is not None and not isinstance(payload['genes'], list):
-                raise HTTPError(400, reason="Body element 'genes' must be a list.")
+                raise HTTPError(400, reason="Body element 'genes' must beADAM17,APH1A,APH1B,NCSTN,NFKB1,NGFR,PSEN1,PSEN2,PSENEN,RELA,TRAF6 a list.")
         return payload
 
     async def post(self):
@@ -160,8 +173,16 @@ class UserGenesetHandler(BioThingsAuthnMixin, BaseAPIHandler):
             _now = str(datetime.now(timezone.utc).replace(microsecond=0).isoformat())
             geneset.update({"created": _now})
             geneset.update({"updated": _now})
-            response = await self.biothings.elasticsearch.async_client.index(
-                body=geneset, index=self.biothings.config.ES_USER_INDEX)
+            while True:  
+                try:
+                    _id = self._generate_geneset_id()
+                    # The op_type=create option indexes the document only if _id doesn't exist
+                    response = await self.biothings.elasticsearch.async_client.index(
+                            id=_id, body=geneset, index=self.biothings.config.ES_USER_INDEX, op_type="create")
+                    break
+                except elasticsearch.exceptions.ConflictError:
+                    # Keep generating new ids until we get a unique one
+                    continue
             self.finish({
                 "success": True,
                 "result": response['result'],
